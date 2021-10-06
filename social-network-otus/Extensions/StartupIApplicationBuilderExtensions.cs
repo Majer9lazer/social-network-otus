986c54339ca66e8db0118a33d3a2b17fefed638e
Egor Sidorenko <sidorenkoegor1999@mail.ru>
2021-10-06 06:48:46 +0300

    added secrets id and connection string log

--- social-network-otus/Extensions/StartupIApplicationBuilderExtensions.cs
+++ social-network-otus/Extensions/StartupIApplicationBuilderExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace social_network_otus.Extensions
 {
@@ -10,7 +11,9 @@ namespace social_network_otus.Extensions
             where TDbContext : DbContext
         {
             using var scope = builder.ApplicationServices.CreateScope();
+            var logger = scope.ServiceProvider.GetRequiredService<ILogger<Startup>>();
             var dbContext = scope.ServiceProvider.GetRequiredService<TDbContext>();
+            logger.LogInformation("DbContext connection string = {0}", dbContext.Database.GetConnectionString());
             dbContext.Database.Migrate();
             return builder;
         }
