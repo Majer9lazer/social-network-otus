e63e2a34f4d82f516627595e949d67de74c36cea
Egor Sidorenko <sidorenkoegor1999@mail.ru>
2021-10-06 08:16:12 +0300

    enable https

--- social-network-otus/Startup.cs
+++ social-network-otus/Startup.cs
@@ -66,7 +66,7 @@ namespace social_network_otus
 
             app.InitDbContext<ApplicationDbContext>();
 
-            //app.UseHttpsRedirection();
+            app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
